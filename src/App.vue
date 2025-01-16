36524882b5a8d7b1ed59976226e0e63ffe21cd8f
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2025-01-16 21:02:53 +0100

    Add the v show condition

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <p>Compteur : {{ count }} </p>
+  <div v-show="count >= 5">Bravo vous avez cliqué plus de 5 fois</div>
   <button @click="increment" >Incrémenter</button>
   <button @click="decrement" >Decrémenter</button>
 </template>
