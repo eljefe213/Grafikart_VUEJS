5ab9975273e6683944408338183b091ed907d15a
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2025-01-19 18:47:00 +0100

    Add the template of the todolist

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
-  <ul>
-    <li>{{ person.firstname}}</li>
-    <li>{{ person.lastname}}</li>
-    <li>{{ person.age}}</li>
-  </ul>
-  <button @click.prevent="randomAge">Changer age</button>
+  <h1>Ma todolist :</h1>
+  <form @submit.prevent="addTodo">
+    <fieldset role="group">
+      <input type="text" placeholder="Tâche à effectuer" v-model="newTodo">
+      <button :disabled="newTodo.length === 0">Ajouter</button>
+    </fieldset>
+  </form>
+  <div v-if="todos.length === 0">Vous n'avez pas de tâches à faire</div>
+  <div v-else>
+    <ul>
+      <li v-for="todo in todos" :key="todo.date">{{ todo.title }}</li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
 import {ref} from "vue";
 
-const person = ref({
-  firstname: 'John',
-  lastname: 'Doe',
-  age: 20
-})
-const randomAge = () => {
-  person.value.age = Math.round(Math.random() * 100)
+const todos = ref([])
+const newTodo = ref('')
+const addTodo = () => {
+  todos.value.push({
+    title: newTodo.value,
+    completed: false,
+    date: Date.now()
+  })
+  newTodo.value = ''
 }
-
 
 </script>
 
