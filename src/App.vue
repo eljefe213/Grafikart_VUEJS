83aea1f9e80404ea89ad223016e22da93bfbc885
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2025-01-19 18:22:44 +0100

    Create an object and manipulate it

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,27 @@
 <template>
-  <p>Compteur : {{ count }}</p>
-  <button @click="increment" >Incrémenter</button>
-  <button @click="decrement" >Decrémenter</button>
-  <hr>
-  <button @click="sortMovie">Réorganiser</button>
-  <form action="" @submit.prevent="addMovie">
-    <input type="text" placeholder="Nouveau film" v-model="movieName">
-    <button>Ajouter</button>
-  </form>
   <ul>
-    <li v-for="movie in movies"
-    :key="movie"> {{ movie }}
-      <button @click="deleteMovie(movie)">Supprimer</button>
-    </li>
+    <li>{{ person.firstname}}</li>
+    <li>{{ person.lastname}}</li>
+    <li>{{ person.age}}</li>
   </ul>
+  <button @click.prevent="randomAge">Changer age</button>
 </template>
 
 <script setup>
 import {ref} from "vue";
 
-const count = ref(0)
-const movieName = ref('')
-const movies = ref([
-    'Matrix',
-    'Lilo & Stitch',
-    'Titanic'
-])
-const increment = (event) => {
-  count.value++
+const person = ref({
+  firstname: 'John',
+  lastname: 'Doe',
+  age: 20
+})
+const randomAge = () => {
+  person.value.age = Math.round(Math.random() * 100)
 }
-const decrement = (event) => {
-  count.value--
-}
-const deleteMovie = (movie) => {
-  movies.value = movies.value.filter(m => m !== movie)
-}
-const sortMovie = () => {
-  movies.value.sort((a,b) => a > b ? 1 : -1)
-}
-const addMovie = () => {
-  movies.value.push(movieName.value)
-  movieName.value = ''
-}
+
+
 </script>
 
 <style>
-.active {
-  color: red;
-}
+
 </style>
