1083d8c7cdbb887d92dbc3b5c13fd738e0985b00
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2025-01-20 21:08:02 +0100

    Add the onChange method

--- src/Checkbox.vue
+++ src/Checkbox.vue
@@ -1,6 +1,6 @@
 <template>
 <label>
-  <input type="checkbox">
+  <input type="checkbox" @change="onChange">
   {{ label }}
 </label>
 </template>
@@ -8,5 +8,12 @@
 defineProps({
   label: String
 })
- const emits = defineEmits(['check', 'uncheck'])
+const emits = defineEmits(['check', 'uncheck'])
+const onChange = (event) => {
+  if (event.currentTarget.checked) {
+    emits('check', event.currentTarget)
+  } else {
+    emits('uncheck', event.currentTarget)
+  }
+}
 </script>
