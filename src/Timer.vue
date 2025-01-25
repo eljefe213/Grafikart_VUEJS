dd5d5886f410a97f4631f555183c88090b788e5b
Eljefe213 <abdelghani.hamaz.pro@gmail.com>
2025-01-25 18:32:44 +0100

    add width/height size

--- src/Timer.vue
+++ src/Timer.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-    Temps : {{ time }}
+  <div ref="div">
+    Temps : {{ time }}<br/>
+    Largeur : {{ size.width }}, Hauteur : {{ size.height }}
   </div>
 </template>
 <script setup>
 import {onMounted, onUnmounted, ref} from "vue";
 
 const time = ref(0)
+const size = ref({width: 0, height: 0})
+const div = ref(null)
+
 let timer
 
 onMounted(() => {
+  const rect = div.value.getBoundingClientRect()
+  size.value = { width: rect.width, height: rect.height }
   timer =   setInterval(() => {
     time.value++
   }, 1_000)
