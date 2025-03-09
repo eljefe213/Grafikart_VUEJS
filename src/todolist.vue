905a965daa00e945595b380aa76ced55d839fc76
Hamaz Abdelghani <101653284+eljefe213@users.noreply.github.com>
2025-03-09 17:04:50 +0100

    clean todolist.vue

--- src/todolist.vue
+++ src/todolist.vue
@@ -1,7 +1,5 @@
 <template>
   <h1>Ma todo-list :</h1>
-  <button @click="showTimer = !showTimer">Afficher / masquer</button>
-  <Timer v-if="showTimer" />
   <form @submit.prevent="addTodo">
     <fieldset role="group">
       <input type="text" placeholder="Tâche à effectuer" v-model="newTodo">
@@ -15,33 +13,16 @@
         <checkbox :label="todo.title" v-model="todo.completed "/>
       </li>
     </ul>
-    <label>
-      <input type="checkbox" v-model="hideCompleted">
-      Masquer les tâches complétées
-    </label>
-    <p v-if="remainingTodos > 0">
-      {{ remainingTodos}} tâche{{ remainingTodos > 1 ? 's' : ''}} à faire
-    </p>
-    <checkbox label="Bonjour"/>
   </div>
 </template>
 
 <script setup>
-import {computed,onMounted, ref} from "vue";
-import Checkbox from './Checkbox.vue';
-import Timer from "@/Timer.vue";
+import {ref} from "vue";
 
 const newTodo = ref('')
-const hideCompleted = ref(false)
 const todos = ref([])
 
 const showTimer = ref(true);
-
-onMounted(() => {
-  fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(r => r.json())
-      .then(v => todos.value = v.map(todo => ({ ...todo, date: todo.id })))
-})
 
 const addTodo = () => {
   todos.value.push({
@@ -52,15 +33,10 @@ const addTodo = () => {
   newTodo.value = ''
 }
 const sortedTodos = computed(() => {
-  const sortedTodos = todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
-  if ( hideCompleted.value === true) {
-    return sortedTodos.filter(t => t.completed === false)
-  }
-  return sortedTodos
+  return todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
+
 })
-const remainingTodos = computed(() => {
-  return todos.value.filter(t => t.completed === false).length
-})
+
 </script>
 
 <style>
